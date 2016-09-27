8a74a33878052c2a4789900a4ba7f8a65532cc69
James <bakerje@mac.com>
2016-09-27 11:52:40 +0100

    Rspec case for Docking Station to release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do
-  #it 'releases bike' do
-  #  expect(docking_station).to eq release_bike
-  #end
+  it 'responds to release_bike' do
+  expect(subject).to respond_to :release_bike
+  end
 end
