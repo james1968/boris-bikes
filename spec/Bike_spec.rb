ddad8da9b4cc4cd7c748958aaf8faae44aeaab5e
James <bakerje@mac.com>
2016-09-27 16:02:20 +0100

    update bike_spec for one line test

--- spec/Bike_spec.rb
+++ spec/Bike_spec.rb
@@ -1,7 +1,7 @@
-require 'bike.rb'
+require 'bike'
 
 describe Bike do
-  it 'should respomd to working?' do
+  it 'should respond to working?' do
     expect(subject).to respond_to :working?
   end
 end
