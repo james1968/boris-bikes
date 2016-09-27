a585ee070b28914d624b5c59bf044716c7c4b445
Glenn <glynester@hotmail.com>
2016-09-27 11:53:52 +0000

    release bike method added. Test added for release_bike method.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,12 @@
 require 'docking_station'
 
 describe DockingStation do
-    
-    
-    
+    it { is_expected.to respond_to :release_bike} 
 end
+
+
+#describe DockingStation do
+#    it 'responds to release_bike' do
+#    expect(subject).to respond_to :release_bike
+#    end
+#end
