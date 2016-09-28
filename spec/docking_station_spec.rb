a07841abd8d376c853a8d69b94d9a1bc27964cf8
James <bakerje@mac.com>
2016-09-28 11:23:35 +0100

    Updated for checking Docking Station Full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -20,16 +20,27 @@ describe DockingStation do
     expect(subject.dock(bike)).to eq bike
   end
 
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+  describe '#dock' do
+
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
+
+    it 'raises an error when full' do
+      subject.dock(bike.new)
+      expect { subject.dock Bike.new }.to raise_error 'Docking Station Full'
+    end
+
   end
 
   describe '#release_bike' do
 
-      it 'raises an  error' do
+      it 'raises an error when empty' do
         expect { subject.release_bike }.to raise_error 'No bike available'
       end
+
+
     end
 end
