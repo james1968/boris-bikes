8d9b53286f68d932c679493d8a387b2ec5518672
crispinandrews <crispin.andrews@gmail.com>
2016-09-28 10:36:58 +0100

    Stage 10 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,28 +1,15 @@
 require 'docking_station'
 
 describe DockingStation do
-    it { is_expected.to respond_to :release_bike} 
-end
+    it { is_expected.to respond_to :release_bike}
 
 
 #release_bike returns a new instance of the Bike class
 #a) gets a bike, and then b) expects the bike to be working
 
 it 'release a bike' do
-    bike = xxx.release_bike
-    expect (bike).to be_working 
+    bike = subject.release_bike
+    expect(bike).to be_working
 end
 
-#describe DockingStation do
-#    it {}
-#    end
-    
-#end
-
-
-
-#describe DockingStation do
-#    it 'responds to release_bike' do
-#    expect(subject).to respond_to :release_bike
-#    end
-#end
+end
