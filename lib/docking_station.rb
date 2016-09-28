431a5c4dce39476e700e3206176d25e606541cfa
crispinandrews <crispin.andrews@gmail.com>
2016-09-28 12:20:32 +0100

    Stage 11 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,13 @@
 require_relative 'bike'
 
 class DockingStation
+attr_accessor :bike
+
     def release_bike
        Bike.new
     end
-    
-    def dock_bike(bike)
-        
-    end
 
+    def dock(bike)
+      @bike = bike
+    end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,7 +12,19 @@ it 'release a bike' do
     expect(bike).to be_working
 end
 
-it {is_expected.to respond_to :dock_bike}
+it {is_expected.to respond_to(:dock).with(1).argument}
 
+it 'bike is docked' do
+  bike = Bike.new
+  expect(subject.dock(bike)).to eq bike
+end
+
+it {is_expected.to respond_to (:bike) }
+
+it 'returns docked bike' do
+  bike = Bike.new
+  subject.dock(bike)
+  expect(subject.bike).to eq bike
+end
 
 end
