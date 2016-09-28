0a7707a67a0789febe4f0a8071cfadc943a84361
Glenn <glynester@hotmail.com>
2016-09-28 08:17:35 +0000

    Start of step 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,6 +5,22 @@ describe DockingStation do
 end
 
 
+#release_bike returns a new instance of the Bike class
+#a) gets a bike, and then b) expects the bike to be working
+
+it 'release a bike' do
+    bike = xxx.release_bike
+    expect (bike).to be_working 
+end
+
+#describe DockingStation do
+#    it {}
+#    end
+    
+#end
+
+
+
 #describe DockingStation do
 #    it 'responds to release_bike' do
 #    expect(subject).to respond_to :release_bike
