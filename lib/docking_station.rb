cf2263c0569c7f3a94ea6b977e6af5cede966804
Tomy8s <tomy8s@hotmail.com>
2016-09-28 17:00:33 +0100

    accepts capacity as argument, but retains DEFAULT_CAPACITY if not given

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,8 +5,8 @@ class DockingStation
 
   DEFAULT_CAPACITY = 20
 
-  def initialize
-    @capacity = DEFAULT_CAPACITY
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
     @bikes = []
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -29,7 +29,7 @@ describe DockingStation do
     end
 
     it 'raises an error when full' do
-      DockingStation::DEFAULT_CAPACITY.times{ subject.dock Bike.new }
+      subject.capacity.times{ subject.dock Bike.new }
       expect { subject.dock Bike.new }.to raise_error 'Docking Station Full'
     end
 
